0e8d2956934f9ca5b32ad37fa77cfdd4542035e5
rory blair <rory.c.blair@gmail.com>
2025-06-27 18:02:02 -0500

    pre-refactor
    this is finally in a state that i'm happy with. it's /just/ enough to /maybe/ be considered a CRUD. very feature bare but that's subject to change in the future.

--- YACRUD.py
+++ YACRUD.py
@@ -1,101 +1,80 @@
-
 '''
-This program is a generic logger. 
-The user will enter whatever they would like to log followed by the quantity and (if applicable).
-This program solves the problem of having to log things to later export to spreadsheets or even a simple text document, while simultaneously being incredibly lightweight.
-This includes, but is certainly not limited to: inventory, logging hours spent doing something, water drank, or logging work miles.
-The user will then be able to search through their log entries, print the log in it's entirety or by entry then copy it to their clipboard for exporting, or tabulate the total entries by the amount.
-This program solves my own personal problem of trying to keep inventory of pre-prepped food items or ingredients in my freezer to then later display on my Raspberry Pi Zero 2 W based Magic Mirror while maintaining a lightweight profile.
-'''
-import sys, pprint
-
-'''
-This function greets the user and begins the entire program
-It allows the program to branch to other functions
+This is yet another CRUD. Very barebones and for broad usage. 
+It's generic scheme allows for future modification to something more specific.
+This solves my own personal problem of wanting a lightweight CRUD to use for inventory management in my household. 
+These inventory changes are then pushed to my Raspberry Pi Zero 2 W based Magic Mirror
+Currently it only stores in memory but the entries can be copied to the clipboard and pasted elsewhere for long term storage.
 '''
 
-parentDict = {}
-childDict = {}
-entryCounter = 1
+import sys
+
+primaryDict = {}
+entryDict = {}
+
+'''
+This is the selection screen where the user can choose from the 3 currently available features.
+I took care to ensure that the input from the user is case-insenstive
+'''
 
 def selectionScreen():
      
     print("Welcome to your personal logging program!\
         \nYou are welcome to log whatever you desire!")
     
-###have choiceLabel print back user choice for both user confirmation and error handling###
- #   choiceLabel = 
- ###refactor lengthy try statement. maybe 1,2,3,4,5?###
- ###could use or operators then###
     while True:
 
         choice = input("[A]dd new entry  \
-                    \n[S]earch log entries \
-                    \n[C]ount log entries \
-                    \n[P]rint log to terminal \
-                    \n[E]xport log \
+                    \n[D]elete entry \
+                    \n[P]rint entries \
                     \n[Q]uit \
                     \nEnter your choice here:\
                     \n").lower()
 
         try: 
             if choice == 'a':
-                createDict(parentDict)
-            elif choice == 's':
-                if parentDict:
-                    searchLog(parentDict)
-            elif choice == 'c':
-                if parentDict:
-                    countLog(parentDict)
+                createData()
+            elif choice == 'd':
+                deleteData(primaryDict)
             elif choice == 'p':
-                if parentDict:
-                    printLog(parentDict)
-            elif choice == 'e':
-                if parentDict:
-                    exportLog(parentDict)
+                printLog(primaryDict)
             elif choice == 'q':
                 sys.exit("Goodbye! See you next time!")
             elif choice == '':
                 print("Please select a valid response.")
                 choice
-            elif choice == 'd':
-                debug()
+                ###remove? does it do anything?###
         except AttributeError:
             print(f"Your choice does not exist yet! Come back later!")
 
-
 '''
-This function will take user input to create a child dictionary which is then nested in a parent dictionary. 
-Each entry is then assigned a key, and value while maintaining a generic labeling scheme.
-The Amount Logged will handle an exception if the user inputs a string as it's for later tabulation.
-The Measurement Unit has a "pseudo execption handling" if the user inputs an integer.
-Each added entry will be printed to the user afterwards for good HCI
+This is where the user creates the dictionary.
+They are offered 2 mandatory, and 1 optional value to input into a dictionary.
+These user dictionaries are then nested into a parent dictionary.
+This is done to ensure that duplicate entries are not overwritten.
+Each nested dictionary's key is based off of the overall entry, and the amount and optional units are values.
 '''
 
-###add quit in this function###
-
-def createDict(parentDict):
-
-        childDict = {}
-  
-        global entryCounter
-
-        childDict["Entry"] = input("Please enter what you would like to log in a list. \
-                        \nWhen you are finished, press enter/return twice to continue to the next portion. \
-                        \nEnter Entry to log here: \
-                        \n"
-                                ).lower().title()
+def createData():
+        
+        entryDict = {}
+###!!!Make this alpha only!!!###
+        entryDict["Entry"] = input("Please enter what you would like to log in a list. \
+                            \nWhen you are finished, press enter/return twice to continue to the next portion. \
+                            \nEnter Entry to log here: \
+                            \n"
+                                ).lower()
 
         while True:
 
             logAmount = input("\nEnter the amount for the item you are logging. \
-                    \nThis needs to be a valid numerical digit \
-                    \nEnter the amount for the entry:"
-                    "\n"
+                        \nThis needs to be a valid numerical digit \
+                        \nEnter the amount for the entry:"
+                        "\n"
                             )
-            
+            ###I took into consideration keeping the amounts as integers for potential functions that can tabulate###
+            ###Exceptions from incorrect entries are handled and users are then guided back to reentry of the amount value###
             try: 
-                childDict["Amount Logged"] = int(logAmount)
+                entryDict["Amount Logged"] = int(logAmount)
                 break
             except ValueError as badInput:
                     print(f"{badInput} is not a valid number. \
@@ -103,95 +82,116 @@ def createDict(parentDict):
 
         while True:
             
-            childDict["Measurement Unit"] = input("Enter the unit measurement if applicable. \
-        \nExample: oz, miles, inches, etc. \
-        \nThis entry must be a word. \
-        \nIf not applicable, press enter/return to skip \
-        \nEnter your measurement here: \
-        \n"
-            )
+            entryDict["Measurement Unit"] = input("Enter the unit measurement if applicable. \
+                                            \nExample: oz, miles, inches, etc. \
+                                            \nThis input must be a word. \
+                                            \nIf not applicable, press enter/return to skip \
+                                            \nEnter your measurement here: \
+                                            \n"
+                                                )
 
-            logUnit = childDict["Measurement Unit"]
+            logUnit = entryDict["Measurement Unit"]
 
             if logUnit == '':
                 break
-            
+            ###"Psuedo exception handling" for incorrect input###
+            ###I couldn't think of a scenario where a unit of measurement was only a digit###
             if logUnit.isalpha():
-                childDict["Measurement Unit"] = logUnit
+                entryDict["Measurement Unit"] = logUnit
                 break
             else:
                 print(f"{logUnit} is invalid. \
                     \nPlease enter a valid unit measurement \
                     \n")
                 continue
-
+        ###The user is then given feedback about their entry for good HCI###
         if logUnit == '':
-            print(f"{childDict['Amount Logged']} {childDict['Entry']} have been added to the log!")
+            print(f"{entryDict['Amount Logged']} {entryDict['Entry']} have been added to the log!")
         else:
-            print(f"{childDict['Entry']}: {childDict['Amount Logged']} {childDict['Measurement Unit']} have been added to the log!")
+            print(f"{entryDict['Entry']}: {entryDict['Amount Logged']} {entryDict['Measurement Unit']} have been added to the log!")
 
-        parentDict[entryCounter] = childDict
-        entryCounter += 1
+        '''
+        This is where the duplicate entries are handeled via a counter starting at 2.
+        The unique key is checked in primaryDictionary. If it already exists, it's given a digit from the counter.
+        If it doesn't exist, it's just entered as normal.
+        I decided on this specific data structure for future features, primarily tabulation and consolidation of duplicate entries.
+        '''
 
-        return parentDict
+        uniqueEntry = entryDict['Entry']
 
-def searchLog(parentDict):
-    print("Feature not implemented! \
-        \nCheck back later! \
-        \nReturning you to the selection screen \
-        \n")
-    return
-
-def countLog(parentDict):
-    print("Feature not implemented! \
-        \nCheck back later! \
-        \nReturning you to the selection screen \
-        \n")
-    return
-
-def mergeParent(parentDict):
-
-    mergedDict = {}
-    for innerDicts in parentDict:
-        innerData = list(innerDicts.values())[0]
-        fullLog = innerData["Full Log"]
-        mergedDict[fullLog] = innerData
-    return mergedDict
-
+        if uniqueEntry in primaryDict:
+            duplicateCounter = 2
+            while True:
+                if f"{uniqueEntry} - {duplicateCounter}" in primaryDict:
+                    duplicateCounter += 1
+                    break
+            duplicateKey = f"{uniqueEntry} - {duplicateCounter}"
+            primaryDict[duplicateKey] = {
+                'Amount Logged': entryDict['Amount Logged'],
+                'Measurement Unit': entryDict['Measurement Unit']
+            }        
+        else:
+            primaryDict[uniqueEntry] = {
+            'Amount Logged': entryDict['Amount Logged'],
+            'Measurement Unit': entryDict['Measurement Unit']
+        }
+        return primaryDict, entryDict
 
 
-##actually do this one##
-def printLog(parentDict):
-    ###placeholder###
-    print("broken")
-    return
+def printLog(primaryDict):
+###This was maybe perhaps overly complicated for what it accomplishes, but I did not aesthetically like the way a "normal" print or pretty print looked###
+    print("Here are your entries so far:")  
+    for key, value in primaryDict.items():
+        sep = " // "
+        print(f"{key}{sep}{value}")
 
-#     print("Welcome to the printing function! \
-#           \nFrom here you can display on the terminal what you have logged thus far")
-#     printChoice = input("Make your selection:  \
-#                     \nPrint [E]ntries \
-#                     \nMore to come!").lower
+'''
+This is where users can delete their entries if they'd like.
+Printing the log is imperative as I'm keeping good HCI practices in mind and I do not know if the user remembers every entry or not.
+Users can also back out of data deletion before it's completed and are given the option to add an entry if it doesn't exist.
+Program feedback is given on deleted entries.
+'''
+
+def deleteData(primaryDict):
     
-#     if printChoice == 'e':
-#         pprint.pprint(parentDict["Entry"])
+    printLog(primaryDict)
 
-#     printEntry
+    while True:
+            
+        choice = input("What entry would you like to delete? \
+                    \nEnter the entry to delete or enter [b]ack to return to selection screen: \
+                    \n"
+        ).title()
+        
+        if choice == 'b':
+            return
+        elif choice in primaryDict.keys():
 
-def exportLog(parentDict):
-    print("Feature not implemented! \
-        \nCheck back later! \
-        \nReturning you to the selection screen \
-        \n")
-    return
+            while True:
 
-def debug():
-###debug section###
-
-###createDict sanity check###
-    pprint.pprint(parentDict)
-
-##mergeParent sanity check###
-    mergedDict = {}
-    pprint.pprint(mergedDict)
+                delConfirm = input(f"Are you sure you want to delete {choice} \
+                                \n[Y]es \
+                                \n[N]o \
+                                \n").lower()
+                if delConfirm == 'y':
+                    del primaryDict[choice]
+                    print(f"{choice} deleted")
+                    break
+                elif delConfirm == 'n':
+                    print(f"You did not delete {choice}")
+                    break
+                
+                addChoice = input(f"{choice} does not exist. Would you like to add it now? \
+                                \n[Y]es \
+                                \n[N]o \
+                                \n").lower()
+                if addChoice == 'y':
+                    createData()
+                elif addChoice == 'n':
+                    break
+        
+        else:
+            print("Invalid choice.")
+            continue
 
 selectionScreen()
