c8174f4fbacff730f81ae3cf907c5bfde8790775
burntpiano <burntpiano@proton.me>
2025-07-02 13:45:15 -0500

    maybe idk what im doing

--- YACRUD.py
+++ YACRUD.py
@@ -14,11 +14,6 @@ These inventory changes are then pushed to my Raspberry Pi Zero 2 W based Magic 
 Currently it only stores in memory but the entries can be copied to the clipboard and pasted elsewhere for long term storage.
 '''
 
-<<<<<<< HEAD
-parentDict = {}
-childDict = {}
-entryCounter = 1
-=======
 import sys
 
 primaryDict = {}
@@ -27,88 +22,11 @@ primaryDict = {}
 This is the selection screen where the user can choose from the 3 currently available features.
 I took care to ensure that the input from the user is case-insensitive
 '''
->>>>>>> cc039e0 (got it lol)
 
 def selectionScreen():
 
     print("Welcome to your personal logging program! \
         \nYou are welcome to log whatever you desire!")
-<<<<<<< HEAD
-    
-###have choiceLabel print back user choice for both user confirmation and error handling###
- #   choiceLabel = 
- ###refactor lengthy try statement. maybe 1,2,3,4,5?###
- ###could use or operators then###
-    while True:
-
-        choice = input("[A]dd new entry  \
-                    \n[S]earch log entries \
-                    \n[C]ount log entries \
-                    \n[P]rint log to terminal \
-                    \n[E]xport log \
-                    \n[Q]uit \
-                    \nEnter your choice here:\
-                    \n").lower()
-
-        try: 
-            if choice == 'a':
-                createDict(parentDict)
-            elif choice == 's':
-                if parentDict:
-                    searchLog(parentDict)
-            elif choice == 'c':
-                if parentDict:
-                    countLog(parentDict)
-            elif choice == 'p':
-                if parentDict:
-                    printLog(parentDict)
-            elif choice == 'e':
-                if parentDict:
-                    exportLog(parentDict)
-            elif choice == 'q':
-                sys.exit("Goodbye! See you next time!")
-            elif choice == '':
-                print("Please select a valid response.")
-                choice
-            elif choice == 'd':
-                debug()
-        except AttributeError:
-            print(f"Your choice does not exist yet! Come back later!")
-
-
-'''
-This function will take user input to create a child dictionary which is then nested in a parent dictionary. 
-Each entry is then assigned a key, and value while maintaining a generic labeling scheme.
-The Amount Logged will handle an exception if the user inputs a string as it's for later tabulation.
-The Measurement Unit has a "pseudo execption handling" if the user inputs an integer.
-Each added entry will be printed to the user afterwards for good HCI
-'''
-
-###add quit in this function###
-
-def createDict(parentDict):
-
-        childDict = {}
-  
-        global entryCounter
-
-        childDict["Entry"] = input("Please enter what you would like to log in a list. \
-                        \nWhen you are finished, press enter/return twice to continue to the next portion. \
-                        \nEnter Entry to log here: \
-                        \n"
-                                ).lower().title()
-
-        while True:
-
-            logAmount = input("\nEnter the amount for the item you are logging. \
-                    \nThis needs to be a valid numerical digit \
-                    \nEnter the amount for the entry:"
-                    "\n"
-                            )
-            
-            try: 
-                childDict["Amount Logged"] = int(logAmount)
-=======
 
     while True:
 
@@ -173,25 +91,12 @@ def createData():
             ###Exceptions from incorrect entries are handled and users are then guided back to reentry of the amount value###
             try:
                 logDict["Amount Logged"] = int(logAmount)
->>>>>>> cc039e0 (got it lol)
                 break
             except ValueError as badInput:
                     print(f"{badInput} is not a valid number. \
                         \nPlease enter a valid number: ")
 
         while True:
-<<<<<<< HEAD
-            
-            childDict["Measurement Unit"] = input("Enter the unit measurement if applicable. \
-        \nExample: oz, miles, inches, etc. \
-        \nThis entry must be a word. \
-        \nIf not applicable, press enter/return to skip \
-        \nEnter your measurement here: \
-        \n"
-            )
-
-            logUnit = childDict["Measurement Unit"]
-=======
 
             logDict["Measurement Unit"] = input("Enter the unit of measurement if applicable. \
                                             \nExample: oz, miles, inches, etc. \
@@ -202,18 +107,13 @@ def createData():
                                                 )
 
             logUnit = logDict["Measurement Unit"]
->>>>>>> cc039e0 (got it lol)
 
             if logUnit == '':
                 break
             ###"Psuedo exception handling" for incorrect input###
             ###I couldn't think of a scenario where a unit of measurement was only a digit###
             if logUnit.isalpha():
-<<<<<<< HEAD
-                childDict["Measurement Unit"] = logUnit
-=======
                 logDict["Measurement Unit"] = logUnit
->>>>>>> cc039e0 (got it lol)
                 break
             else:
                 print(f"{logUnit} is invalid. \
@@ -222,12 +122,6 @@ def createData():
                     \n")
                 continue
 
-<<<<<<< HEAD
-        if logUnit == '':
-            print(f"{childDict['Amount Logged']} {childDict['Entry']} have been added to the log!")
-        else:
-            print(f"{childDict['Entry']}: {childDict['Amount Logged']} {childDict['Measurement Unit']} have been added to the log!")
-=======
 
         ###The user is then given feedback about their entry###
         ###It unfortunately won't always be grammatically sound, but I think it still gets the message across###
@@ -235,75 +129,9 @@ def createData():
             print(f"{logDict['Amount Logged']} {logDict['Entry']} have been added to the log!")
         else:
             print(f"{logDict['Entry']}: {logDict['Amount Logged']} {logDict['Measurement Unit']} have been added to the log!")
->>>>>>> cc039e0 (got it lol)
-
-        parentDict[entryCounter] = childDict
-        entryCounter += 1
-
-        return parentDict
-
-<<<<<<< HEAD
-def searchLog(parentDict):
-    print("Feature not implemented! \
-        \nCheck back later! \
-        \nReturning you to the selection screen \
-        \n")
-    return
-
-def countLog(parentDict):
-    print("Feature not implemented! \
-        \nCheck back later! \
-        \nReturning you to the selection screen \
-        \n")
-    return
-
-def mergeParent(parentDict):
-
-    mergedDict = {}
-    for innerDicts in parentDict:
-        innerData = list(innerDicts.values())[0]
-        fullLog = innerData["Full Log"]
-        mergedDict[fullLog] = innerData
-    return mergedDict
 
 
 
-##actually do this one##
-def printLog(parentDict):
-    ###placeholder###
-    print("broken")
-    return
-
-#     print("Welcome to the printing function! \
-#           \nFrom here you can display on the terminal what you have logged thus far")
-#     printChoice = input("Make your selection:  \
-#                     \nPrint [E]ntries \
-#                     \nMore to come!").lower
-    
-#     if printChoice == 'e':
-#         pprint.pprint(parentDict["Entry"])
-
-#     printEntry
-
-def exportLog(parentDict):
-    print("Feature not implemented! \
-        \nCheck back later! \
-        \nReturning you to the selection screen \
-        \n")
-    return
-
-def debug():
-###debug section###
-
-###createDict sanity check###
-    pprint.pprint(parentDict)
-
-##mergeParent sanity check###
-    mergedDict = {}
-    pprint.pprint(mergedDict)
-
-selectionScreen()
-=======
         duplicateCheck(primaryDict, logDict)
 
         return logDict, primaryDict
@@ -394,4 +222,3 @@ I went through several different revisions of differing complexity before decidi
 Deciding on how I wanted to structure my database was difficult as it is leveraged everywhere else and integral for functionality.
 '''
 selectionScreen()
->>>>>>> cc039e0 (got it lol)
