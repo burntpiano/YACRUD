2c46939ef49ac9b555a7e371d4af442036f500ca
burntpiano <burntpiano@proton.me>
2025-07-05 14:41:03 -0500

    added save/load functions to/from json file type.

--- YACRUD.py
+++ YACRUD.py
@@ -1,7 +1,7 @@
 '''
 INF360 - Programming in Python
 
-Midterm Project
+Final Project
 
 I, Rory Blair , affirm that the work submitted for this assignment is entirely my own. I have not engaged in any form of academic dishonesty, including but not limited to cheating, plagiarism, or the use of unauthorized materials. This includes, but is not limited to, the use of resources such as Chegg, MyCourseHero, StackOverflow, ChatGPT, or other AI assistants, except where explicitly permitted by the instructor. I have neither provided nor received unauthorized assistance and have accurately cited all sources in adherence to academic standards. I understand that failing to comply with this integrity statement may result in consequences, including disciplinary actions as determined by my course instructor and outlined in institutional policies. By signing this statement, I acknowledge my commitment to upholding the principles of academic integrity.
 '''
@@ -14,7 +14,15 @@ These inventory changes are then pushed to my Raspberry Pi Zero 2 W based Magic 
 Currently it only stores in memory but the entries can be copied to the clipboard and pasted elsewhere for long term storage.
 '''
 
-import sys
+import sys, os, json, pprint
+
+###Global###
+from pathlib import Path
+from datetime import date
+d = date.today()
+USYearMonth = d.strftime("%Y-%m")
+currentDay = d.strftime("%A %d")
+saveDir = (Path.cwd() / 'Saved Logs' / USYearMonth)
 
 primaryDict = {}
 
@@ -33,17 +41,23 @@ def selectionScreen():
         choice = input("[A]dd new entry  \
                         \n[D]elete entry \
                         \n[P]rint entries \
+                        \n[S]ave log \
+                        \n[L]oad Log \
                         \n[Q]uit \
                         \nEnter your choice here:\
                         \n"
                        ).lower()
 
         if choice == 'a':
-            createData()
+            createData(primaryDict)
         elif choice == 'd':
             deleteData(primaryDict)
         elif choice == 'p':
             printLog(primaryDict)
+        elif choice == 's':
+            saveLog()
+        elif choice == 'l':
+            loadLog()
         elif choice == 'q':
             sys.exit("Goodbye! See you next time!")
         else:
@@ -57,7 +71,7 @@ This is done to ensure that duplicate entries are not overwritten.
 Each nested dictionary's key is based off of the overall entry, and the amount and optional units are values.
 '''
 
-def createData():
+def createData(primaryDict):
 
         logDict = {}
 
@@ -173,7 +187,9 @@ def printLog(primaryDict):
     for key, value in primaryDict.items():
         sep = " // "
         print(f"{key}{sep}{value}")
-
+        global printedLog
+        printedLog = f"{key}{sep}{value}"
+    return 
 
 '''
 This is where users can delete their entries if they'd like.
@@ -216,9 +232,91 @@ def deleteData(primaryDict):
             print("Invalid choice.")
             continue
 
+def saveLog():
+
+    printLog(primaryDict)
+
+    from pathlib import Path
+    saveDir = (Path.cwd() / 'Saved Logs' / USYearMonth)
+    saveDir.mkdir(parents=True, exist_ok=True)
+
+    saveInput = input("Save your log so far?: \
+                      \n[Y]es \
+                      \n[N]o \
+                      \n").lower()
+
+
+    try:
+        if saveInput == 'y':
+            userLogName = input ("Use current day as the log name? \
+                                \n[Y]es \
+                                \n[N]o \
+                                \n").lower()
+            if userLogName == 'y':
+                fileName = currentDay
+                logFile = Path(saveDir / f"{fileName}.json")
+                with logFile.open('w') as ldf:
+                    json.dump(primaryDict, ldf, indent=4)
+                    pprint.pprint("Log saved as " f"{fileName} in {saveDir}"".")
+                if isinstance(logFile, dict):
+                    logFile.append(primaryDict)
+            if userLogName == 'n':
+                userLogInput = input("Type what you would like to save this log as: \
+                            \n")
+                fileName = userLogInput
+                logFile = Path(saveDir / f"{fileName}.json")
+                with logFile.open('w') as lnf:
+                        json.dump(primaryDict, lnf, indent=4)
+                        print(f"Log saved as {userLogInput} in {saveDir}.")
+            if saveInput == 'n':
+                print("Returning you to the selection screen.")
+                return
+    except FileNotFoundError:
+        ###fix this one, obviously###
+        print("Lemme figure this one out real quick.")
+
+        fileName = userLogInput, currentDay
+        logFile = Path(saveDir / f"{fileName}.json")
+        
+    
+    return saveDir, logFile
+
+def loadLog():
+
+    global primaryDict
+    
+    logFiles = list(saveDir.glob('*.json'))
+    sortedFiles = sorted(logFiles, key=os.path.getmtime)
+    ##consider adding pages for long lists of directories/files?##
+    ###lots of dirs/files will make for a long list###
+
+    if not logFiles:
+        print("No logs found")
+    print("Select a file to load: ")
+    while True:
+        for index, file in enumerate(sortedFiles, 1):
+            print(f"[{index}] {file}")
+
+        try:
+            loadInput = int(input("Enter the corresponding number of the log you would like to load: \
+                            \n"))
+            loadedFile = logFiles[loadInput - 1]
+            with loadedFile.open('r') as lf:
+                primaryDict = json.load(lf)
+                print(f"{primaryDict} loaded.")
+                return primaryDict
+        except FileNotFoundError:
+            print("Log not found. Please try again.")
+            continue
+        
+        return primaryDict
+
+###create a delete file function###
+
 '''
 Finally, it all executes from a single function.
 I went through several different revisions of differing complexity before deciding on this.
 Deciding on how I wanted to structure my database was difficult as it is leveraged everywhere else and integral for functionality.
 '''
+
 selectionScreen()
